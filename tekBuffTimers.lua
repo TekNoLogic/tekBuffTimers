6097bd08b8f7723dc71a877a4a3f14b8e1946f12
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-09-29 01:59:03 -0600

    Update for Legion (buffs that last DAYS)

--- tekBuffTimers.lua
+++ tekBuffTimers.lua
@@ -1,15 +1,19 @@
 
-SecondsToTimeAbbrev = function(time)
-	local hr, m, s, text
-	if time <= 0 then text = ""
-	elseif time < 3600 then
-		m = floor(time / 60)
-		s = mod(time, 60)
-		text = (m == 0 and format("%d", s)) or format("%d:%02d", m, s)
-	else
-		hr = floor(time / 3600)
-		m = floor(mod(time, 3600) / 60)
-		text = format("%d.%2d h", hr, m)
-	end
-	return text
+local SECONDS_PER_MINUTE = 60
+local SECONDS_PER_HOUR   = 60 * SECONDS_PER_MINUTE
+local SECONDS_PER_DAY    = 24 * SECONDS_PER_HOUR
+
+function SecondsToTimeAbbrev(seconds)
+	if seconds <= 0 then return "" end
+
+	local days = seconds / SECONDS_PER_DAY
+	if days >= 1 then return string.format("%.1fd", days) end
+
+	local hours = seconds / SECONDS_PER_HOUR
+	if hours >= 1 then return string.format("%.02fh", hours) end
+
+	local minutes = seconds / SECONDS_PER_MINUTE
+	local seconds = seconds % SECONDS_PER_MINUTE
+	if minutes >= 1 then return string.format("%d:%02d", minutes, seconds) end
+	return string.format("%ds", seconds)
 end
